168a0685362bae34ef11351b0e4ed30b3d9cc796
simonewebdesign <hello@simonewebdesign.it>
2015-11-25 22:46:36 +0000

    Make square

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,49 @@
 module Main where
 
-import Html exposing (Html, text)
+import Graphics.Element as Element
+import Graphics.Collage as Collage
+import Text
+import Color exposing (Color, rgb)
 
-main : Html
+
+red : Color
+red =
+  rgb 255 0 0
+
+
+green : Color
+green =
+  rgb 0 255 0
+
+
+makeSquare : Collage.Form
+makeSquare =
+  Collage.square 30
+  |> Collage.filled green
+
+
+aTextElement : Element.Element
+aTextElement =
+  Element.centered (Text.fromString "Hi there")
+
+
+redSquare : Element.Element
+redSquare =
+  Element.color red aTextElement
+
+
+myCollage : List Collage.Form -> Element.Element
+myCollage forms =
+  Collage.collage 640 480 forms
+
+
+myForms : List Collage.Form
+myForms =
+  [ Collage.toForm redSquare
+  , makeSquare
+  ]
+
+
+main : Element.Element
 main =
-  text "Hello World"
+  myCollage myForms
