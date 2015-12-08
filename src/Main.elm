d3b3eac2cc4d0d0fe26fda2c6494147fa6f85d6c
simonewebdesign <hello@simonewebdesign.it>
2015-12-08 22:13:12 +0000

    Changes (wip)

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,36 @@ import Graphics.Element as Element
 import Graphics.Collage as Collage
 import Text
 import Color exposing (Color, rgb)
+import Time
+import Keyboard
+--import Window
 
+-- MODEL
+
+type alias Model =
+  { color : Color
+  , counter : Int
+  }
+
+
+type alias Keys = { x:Int, y:Int }
+
+
+model : Model
+model =
+  { color = red
+  , counter = 0
+  }
+
+
+-- UPDATE
+
+update : Model -> Model
+update model =
+  model
+
+
+-- VIEW
 
 red : Color
 red =
@@ -44,6 +73,28 @@ myForms =
   ]
 
 
-main : Element.Element
-main =
+view : Model -> Element.Element
+view model =
   myCollage myForms
+
+
+-- MAIN
+
+--main : Element.Element
+--main =
+--  myCollage myForms
+
+
+-- SIGNALS
+
+main : Signal Element.Element
+main =
+  Signal.map2 view Signal model (Signal.foldp update model input)
+
+
+input : Signal (Float, Keys)
+input =
+  let
+    delta = Signal.map (\t -> t/20) (Time.fps 30)
+  in
+    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
