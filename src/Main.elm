531387493e0ead166201ed622ff99e822916f8bc
simonewebdesign <hello@simonewebdesign.it>
2015-12-21 12:09:53 +0000

    Cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,6 @@ module Main where
 import Array exposing (Array)
 import Graphics.Element as Element exposing (Element)
 import Graphics.Collage as Collage
-import Text
 import Color exposing (Color)
 import Time
 import Keyboard
@@ -53,69 +52,9 @@ update action model =
 
 -- VIEW
 
-makeSquare : Collage.Form
-makeSquare =
-  Collage.square 30
-  |> Collage.filled Color.green
-
-
-aTextElement : Element
-aTextElement =
-  Element.centered (Text.fromString "Hi there")
-
-
-redSquare : Element
-redSquare =
-  Element.color Color.red aTextElement
-
-
-drawPositionedSquare : Dimensions -> Position -> Color -> Collage.Form
-drawPositionedSquare (width, height) (x, y) color =
-  Collage.square (toFloat (height // 2))
-  |> Collage.filled color
-  |> Collage.move (toFloat x, toFloat y)
-
-
-drawSquare : Dimensions -> Color -> Collage.Form
-drawSquare (w, h) color =
-  let
-    dimensions = toFloat (h // 2)
-  in
-    Collage.square dimensions
-    |> Collage.filled color
-
-
-drawSquareElement : Dimensions -> Color -> Element
-drawSquareElement (w, h) color =
-  let
-    width = w // 2
-    height = h // 2
-  in
-    Element.empty
-    |> Element.size width height
-    |> Element.color color
-
-
-aContainer : Element
-aContainer =
-  Element.container 300 300 Element.topLeft redSquare
-  |> Element.color Color.blue
-
-
-formSquare : Dimensions -> Color -> Element.Position -> Collage.Form
-formSquare (w, h) color position =
-  drawSquareElement (w, h) color
-  |> Element.container w h position
-  |> Collage.toForm
-
-
 view : Dimensions -> Model -> Element
 view (w, h) model =
   Collage.collage w h
-    --[ drawSquare (w, h) ((-w // 2), (h // 2)) Color.red
-    --, drawSquare (w, h) ((-w // 2), (-h // 2)) Color.yellow
-    --, drawSquare (w, h) ((w // 2), (h // 2)) Color.green
-    --, drawSquare (w, h) ((w // 2), (-h // 2)) Color.blue
     [ formSquare (w, h) Color.red    Element.topLeft
     , formSquare (w, h) Color.yellow Element.topRight
     , formSquare (w, h) Color.green  Element.bottomLeft
@@ -123,6 +62,24 @@ view (w, h) model =
     , Collage.toForm (Element.show model)
       |> Collage.moveY 100  -- debugging
     ]
+
+
+formSquare : Dimensions -> Color -> Element.Position -> Collage.Form
+formSquare (w, h) color position =
+  drawSquare (w, h) color
+  |> Element.container w h position
+  |> Collage.toForm
+
+
+drawSquare : Dimensions -> Color -> Element
+drawSquare (w, h) color =
+  let
+    width = w // 2
+    height = h // 2
+  in
+    Element.empty
+    |> Element.size width height
+    |> Element.color color
 
 
 -- MAIN
