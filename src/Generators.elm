f8fdcf0f4292bbaf5530450cec6ff41cfed16288
simonewebdesign <hello@simonewebdesign.it>
2016-01-02 17:03:22 +0000

    Refactoring generators

--- src/Generators.elm
+++ src/Generators.elm
@@ -1,3 +1,45 @@
-module Generators where
+module Generators (initialSequence, randomID) where
 
 import Random
+import Array exposing (Array)
+
+type alias ID = Int
+
+initialSequence : Array ID
+initialSequence =
+  let
+    listGenerator = Random.list 4 randomIDgenerator
+    seed = Random.initialSeed 123 -- not random, always generates [2,4,2,4]
+    (list, _) = Random.generate listGenerator seed
+  in
+    Array.fromList list
+
+
+randomID : Random.Seed -> (ID, Random.Seed)
+randomID seed =
+  Random.generate randomIDgenerator seed
+
+
+randomIDgenerator : Random.Generator ID
+randomIDgenerator =
+  Random.int 1 4
+
+  --let
+  --  (id, newSeed) = randomId model.seed
+  --in
+  --Array.repeat 4 randomID
+  --Array.foldl randomID Array.empty
+  --Array.foldl (\elem acc ->
+  --  let
+  --    (id, newSeed) = randomId
+  --) Array.empty
+
+
+--pop : Array a -> a
+--pop arr =
+--  let [value] = Array.slice 0 -1 arr
+--  in value
+-- Returns the array with only the last button.
+--last : Array a -> Array a
+--last a =
+--  Array.slice -1 (Array.length a) a
--- src/Main.elm
+++ src/Main.elm
@@ -12,7 +12,7 @@ import Mouse
 import Random
 import Maybe
 import Text
---import Generators
+import Generators
 
 -- MODEL
 
@@ -30,11 +30,13 @@ type alias Model =
   }
 
 type alias ID = Int
+
 type alias Button =
   { pressed : Bool
   , position : Position
   , color : Color
   }
+
 type alias Dimensions = (Int, Int)
 type alias Position = (Float, Float)
 
@@ -61,61 +63,21 @@ initialModel =
 
   , level = 1
   , score = 0
-  , sequence = initialSequence
+  , sequence = Generators.initialSequence
   , inputSequence = Array.empty
   , seed = Random.initialSeed 111
   , state = Pause
   , buttons =
-    [ ( 1, { pressed = False, position = (-200, 160), color = red } )
-    , ( 2, { pressed = False, position = (200, 160), color = yellow } )
+    [ ( 1, { pressed = False, position = (-200, 160),  color = red } )
+    , ( 2, { pressed = False, position = (200, 160),   color = yellow } )
     , ( 3, { pressed = False, position = (-200, -160), color = green } )
-    , ( 4, { pressed = False, position = (200, -160), color = blue } )
+    , ( 4, { pressed = False, position = (200, -160),  color = blue } )
     ]
   }
 
 
 buttonIDs : Signal.Mailbox Int
 buttonIDs = Signal.mailbox 0
-
-
-randomIDgenerator : Random.Generator ID
-randomIDgenerator =
-  Random.int 1 4
-
-
-randomID : Random.Seed -> (ID, Random.Seed)
-randomID seed =
-  Random.generate randomIDgenerator seed
-
-
-initialSequence : Array ID
-initialSequence =
-  let
-    listGenerator = Random.list 4 randomIDgenerator
-    seed = Random.initialSeed 123 -- not random, always generates [2,4,2,4]
-    (list, _) = Random.generate listGenerator seed
-  in
-    Array.fromList list
-
-  --let
-  --  (id, newSeed) = randomId model.seed
-  --in
-  --Array.repeat 4 randomID
-  --Array.foldl randomID Array.empty
-  --Array.foldl (\elem acc ->
-  --  let
-  --    (id, newSeed) = randomId
-  --) Array.empty
-
-
---pop : Array a -> a
---pop arr =
---  let [value] = Array.slice 0 -1 arr
---  in value
--- Returns the array with only the last button.
---last : Array a -> Array a
---last a =
---  Array.slice -1 (Array.length a) a
 
 
 -- UPDATE
@@ -127,7 +89,6 @@ update action model =
     Subtract -> { model | counter = model.counter - 1 }
 
     Press id ->
-      -- if model.inputSequence.length == model.sequence.length then
       let
         index = Array.length model.inputSequence
         sequenceID = Maybe.withDefault 0 (Array.get index model.sequence)
@@ -205,7 +166,7 @@ updateInputSequence id model =
 newSequence : Model -> Model
 newSequence model =
   let
-    (newID, newSeed) = randomID model.seed
+    (newID, newSeed) = Generators.randomID model.seed
   in
   { model
     | sequence = Array.push newID model.sequence
